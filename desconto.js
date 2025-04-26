b3f6b685b586d42192d99d176e150769f674b9b8
Anderson Gouveia Lignelli <98903039+AndersonGL@users.noreply.github.com>
2025-04-26 14:39:20 -0300

    update js

--- desconto.js
+++ desconto.js
@@ -1,1 +1,6 @@
-// console.log("Ola,Mundo!")
+// Taggle significa alternar entre dois estados posssiveis.E´ frequentemente usado em interfaces de ususa´rio e programação ara representar algo que epode ser"ligadoo/desliagdo" ou "ativo/inativo.
+
+// Função para alternar a exibiçao di formulário de cupom
+
+// <i class= 'bx bx-chevron-up><i></i> - ICONE DA SETA PRA CIMA
+
